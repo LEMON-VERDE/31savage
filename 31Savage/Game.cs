202f32a0c626d4619a73ce34b233584067732250
Anthony Parris <para2709@usherbrooke.ca>
2017-11-11 13:25:47 -0500

    add functions to Game class

--- 31Savage/Game.cs
+++ 31Savage/Game.cs
@@ -14,14 +14,34 @@ namespace _31Savage
         public List<User> players;
         private User currentPlayer;
         private User playerWhoKnocked;
+        private List<User>.Enumerator listIterator;
+        private bool hasSetListIterator = false;
 
+        //Game
+        public void initGame() { }
+
+        //List of players
+        private void setListIterator() { listIterator = players.GetEnumerator();
+                                        listIterator.MoveNext();}
+        public void initListIterator() { if (!hasSetListIterator) { setListIterator();
+                                                                     hasSetListIterator = true;} }
+
+        //Player
         public void setCurrentPlayer(User player) { currentPlayer = player; }
         public User getCurrentPlayer(){ return currentPlayer; }
         public void setPlayerWhoKnocked(User player) { playerWhoKnocked = player; }
         public User getPlayerWhoKnocked() { return playerWhoKnocked; }
 
+        //public void currentPlayerPlayed() { setCurrentPlayer(players) }
+
+        //Card
         public Card getLastThrownCard() { return throwAwayPile.Pop(); }
         public Card checkLastThrownCard() { return throwAwayPile.Peek(); }
         public void throwACard(Card card) { throwAwayPile.Push(card); }
+
+        //Rounds and turns
+        //public bool isGameDone() { if (getCurrentPlayer() == getPlayerWhoKnocked() ||
+       //                                                                             )
+        //                            { return true; } }
     }
 }
