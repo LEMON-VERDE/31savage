a06da6ac276268fb4bbfe221ed6dcd71dd71a1fe
Anthony Parris <para2709@usherbrooke.ca>
2017-11-11 12:48:59 -0500

    add funcitons for Game Class

--- 31Savage/Game.cs
+++ 31Savage/Game.cs
@@ -8,10 +8,20 @@ namespace _31Savage
 {
     class Game
     {
+        public int round;
         public int cardsInDrawPile = 52;
         public Stack<Card> throwAwayPile;
         public List<User> players;
+        private User currentPlayer;
+        private User playerWhoKnocked;
 
+        public void setCurrentPlayer(User player) { currentPlayer = player; }
+        public User getCurrentPlayer(){ return currentPlayer; }
+        public void setPlayerWhoKnocked(User player) { playerWhoKnocked = player; }
+        public User getPlayerWhoKnocked() { return playerWhoKnocked; }
 
+        public Card getLastThrownCard() { return throwAwayPile.Pop(); }
+        public Card checkLastThrownCard() { return throwAwayPile.Peek(); }
+        public void throwACard(Card card) { throwAwayPile.Push(card); }
     }
 }
