810869162fb3edbc92ccc6a99f16cc95f6e76a1b
Édouard Denommée <dene2303@usherbrooke.ca>
2017-11-11 13:35:23 -0500

    Added Simon's UI

---
+++ 31Savage/Login.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace _31Savage
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_start = new System.Windows.Forms.Button();
+            this.lbl_Title = new System.Windows.Forms.Label();
+            this.lbl_Usr1 = new System.Windows.Forms.Label();
+            this.lbl_Usr2 = new System.Windows.Forms.Label();
+            this.lbl_Usr3 = new System.Windows.Forms.Label();
+            this.lbl_Usr4 = new System.Windows.Forms.Label();
+            this.lbl_Usr5 = new System.Windows.Forms.Label();
+            this.lbl_Usr6 = new System.Windows.Forms.Label();
+            this.lbl_Usr7 = new System.Windows.Forms.Label();
+            this.lbl_Usr8 = new System.Windows.Forms.Label();
+            this.lbl_Usr9 = new System.Windows.Forms.Label();
+            this.lbl_NomUsr1 = new System.Windows.Forms.Label();
+            this.txtBox_NomUsr2 = new System.Windows.Forms.TextBox();
+            this.txtBox_NomUsr3 = new System.Windows.Forms.TextBox();
+            this.txtBox_NomUsr4 = new System.Windows.Forms.TextBox();
+            this.txtBox_NomUsr5 = new System.Windows.Forms.TextBox();
+            this.txtBox_NomUsr6 = new System.Windows.Forms.TextBox();
+            this.txtBox_NomUsr7 = new System.Windows.Forms.TextBox();
+            this.txtBox_NomUsr8 = new System.Windows.Forms.TextBox();
+            this.txtBox_NomUsr9 = new System.Windows.Forms.TextBox();
+            this.btn_nav = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.AutoSize = true;
+            this.btn_start.Font = new System.Drawing.Font("Eras Demi ITC", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_start.Location = new System.Drawing.Point(390, 210);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(164, 36);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "Start a Game";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // lbl_Title
+            // 
+            this.lbl_Title.AutoSize = true;
+            this.lbl_Title.Font = new System.Drawing.Font("Lao UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Title.Location = new System.Drawing.Point(381, 64);
+            this.lbl_Title.Name = "lbl_Title";
+            this.lbl_Title.Size = new System.Drawing.Size(203, 54);
+            this.lbl_Title.TabIndex = 1;
+            this.lbl_Title.Text = "31 Savage";
+            // 
+            // lbl_Usr1
+            // 
+            this.lbl_Usr1.AutoSize = true;
+            this.lbl_Usr1.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Usr1.Location = new System.Drawing.Point(278, 135);
+            this.lbl_Usr1.Name = "lbl_Usr1";
+            this.lbl_Usr1.Size = new System.Drawing.Size(86, 19);
+            this.lbl_Usr1.TabIndex = 2;
+            this.lbl_Usr1.Text = "Player 1 :";
+            this.lbl_Usr1.Visible = false;
+            // 
+            // lbl_Usr2
+            // 
+            this.lbl_Usr2.AutoSize = true;
+            this.lbl_Usr2.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Usr2.Location = new System.Drawing.Point(278, 165);
+            this.lbl_Usr2.Name = "lbl_Usr2";
+            this.lbl_Usr2.Size = new System.Drawing.Size(86, 19);
+            this.lbl_Usr2.TabIndex = 3;
+            this.lbl_Usr2.Text = "Player 2 :";
+            this.lbl_Usr2.Visible = false;
+            // 
+            // lbl_Usr3
+            // 
+            this.lbl_Usr3.AutoSize = true;
+            this.lbl_Usr3.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Usr3.Location = new System.Drawing.Point(278, 195);
+            this.lbl_Usr3.Name = "lbl_Usr3";
+            this.lbl_Usr3.Size = new System.Drawing.Size(86, 19);
+            this.lbl_Usr3.TabIndex = 4;
+            this.lbl_Usr3.Text = "Player 3 :";
+            this.lbl_Usr3.Visible = false;
+            // 
+            // lbl_Usr4
+            // 
+            this.lbl_Usr4.AutoSize = true;
+            this.lbl_Usr4.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Usr4.Location = new System.Drawing.Point(278, 225);
+            this.lbl_Usr4.Name = "lbl_Usr4";
+            this.lbl_Usr4.Size = new System.Drawing.Size(86, 19);
+            this.lbl_Usr4.TabIndex = 5;
+            this.lbl_Usr4.Text = "Player 4 :";
+            this.lbl_Usr4.Visible = false;
+            // 
+            // lbl_Usr5
+            // 
+            this.lbl_Usr5.AutoSize = true;
+            this.lbl_Usr5.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Usr5.Location = new System.Drawing.Point(278, 255);
+            this.lbl_Usr5.Name = "lbl_Usr5";
+            this.lbl_Usr5.Size = new System.Drawing.Size(86, 19);
+            this.lbl_Usr5.TabIndex = 6;
+            this.lbl_Usr5.Text = "Player 5 :";
+            this.lbl_Usr5.Visible = false;
+            // 
+            // lbl_Usr6
+            // 
+            this.lbl_Usr6.AutoSize = true;
+            this.lbl_Usr6.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Usr6.Location = new System.Drawing.Point(278, 285);
+            this.lbl_Usr6.Name = "lbl_Usr6";
+            this.lbl_Usr6.Size = new System.Drawing.Size(86, 19);
+            this.lbl_Usr6.TabIndex = 7;
+            this.lbl_Usr6.Text = "Player 6 :";
+            this.lbl_Usr6.Visible = false;
+            // 
+            // lbl_Usr7
+            // 
+            this.lbl_Usr7.AutoSize = true;
+            this.lbl_Usr7.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Usr7.Location = new System.Drawing.Point(278, 315);
+            this.lbl_Usr7.Name = "lbl_Usr7";
+            this.lbl_Usr7.Size = new System.Drawing.Size(86, 19);
+            this.lbl_Usr7.TabIndex = 8;
+            this.lbl_Usr7.Text = "Player 7 :";
+            this.lbl_Usr7.Visible = false;
+            // 
+            // lbl_Usr8
+            // 
+            this.lbl_Usr8.AutoSize = true;
+            this.lbl_Usr8.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Usr8.Location = new System.Drawing.Point(278, 345);
+            this.lbl_Usr8.Name = "lbl_Usr8";
+            this.lbl_Usr8.Size = new System.Drawing.Size(86, 19);
+            this.lbl_Usr8.TabIndex = 9;
+            this.lbl_Usr8.Text = "Player 8 :";
+            this.lbl_Usr8.Visible = false;
+            // 
+            // lbl_Usr9
+            // 
+            this.lbl_Usr9.AutoSize = true;
+            this.lbl_Usr9.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Usr9.Location = new System.Drawing.Point(278, 375);
+            this.lbl_Usr9.Name = "lbl_Usr9";
+            this.lbl_Usr9.Size = new System.Drawing.Size(86, 19);
+            this.lbl_Usr9.TabIndex = 10;
+            this.lbl_Usr9.Text = "Player 9 :";
+            this.lbl_Usr9.Visible = false;
+            // 
+            // lbl_NomUsr1
+            // 
+            this.lbl_NomUsr1.AutoSize = true;
+            this.lbl_NomUsr1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_NomUsr1.Location = new System.Drawing.Point(442, 135);
+            this.lbl_NomUsr1.Name = "lbl_NomUsr1";
+            this.lbl_NomUsr1.Size = new System.Drawing.Size(93, 16);
+            this.lbl_NomUsr1.TabIndex = 11;
+            this.lbl_NomUsr1.Text = "SavageBoiii";
+            this.lbl_NomUsr1.Visible = false;
+            // 
+            // txtBox_NomUsr2
+            // 
+            this.txtBox_NomUsr2.Location = new System.Drawing.Point(445, 164);
+            this.txtBox_NomUsr2.Name = "txtBox_NomUsr2";
+            this.txtBox_NomUsr2.Size = new System.Drawing.Size(139, 20);
+            this.txtBox_NomUsr2.TabIndex = 12;
+            this.txtBox_NomUsr2.Visible = false;
+            this.txtBox_NomUsr2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtBox_NomUsr2_KeyUp);
+            // 
+            // txtBox_NomUsr3
+            // 
+            this.txtBox_NomUsr3.Location = new System.Drawing.Point(445, 194);
+            this.txtBox_NomUsr3.Name = "txtBox_NomUsr3";
+            this.txtBox_NomUsr3.Size = new System.Drawing.Size(139, 20);
+            this.txtBox_NomUsr3.TabIndex = 13;
+            this.txtBox_NomUsr3.Visible = false;
+            this.txtBox_NomUsr3.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtBox_NomUsr3_KeyUp);
+            // 
+            // txtBox_NomUsr4
+            // 
+            this.txtBox_NomUsr4.Location = new System.Drawing.Point(445, 224);
+            this.txtBox_NomUsr4.Name = "txtBox_NomUsr4";
+            this.txtBox_NomUsr4.Size = new System.Drawing.Size(139, 20);
+            this.txtBox_NomUsr4.TabIndex = 14;
+            this.txtBox_NomUsr4.Visible = false;
+            this.txtBox_NomUsr4.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtBox_NomUsr4_KeyUp);
+            // 
+            // txtBox_NomUsr5
+            // 
+            this.txtBox_NomUsr5.Location = new System.Drawing.Point(445, 254);
+            this.txtBox_NomUsr5.Name = "txtBox_NomUsr5";
+            this.txtBox_NomUsr5.Size = new System.Drawing.Size(139, 20);
+            this.txtBox_NomUsr5.TabIndex = 15;
+            this.txtBox_NomUsr5.Visible = false;
+            this.txtBox_NomUsr5.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtBox_NomUsr5_KeyUp);
+            // 
+            // txtBox_NomUsr6
+            // 
+            this.txtBox_NomUsr6.Location = new System.Drawing.Point(445, 284);
+            this.txtBox_NomUsr6.Name = "txtBox_NomUsr6";
+            this.txtBox_NomUsr6.Size = new System.Drawing.Size(139, 20);
+            this.txtBox_NomUsr6.TabIndex = 16;
+            this.txtBox_NomUsr6.Visible = false;
+            this.txtBox_NomUsr6.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtBox_NomUsr6_KeyUp);
+            // 
+            // txtBox_NomUsr7
+            // 
+            this.txtBox_NomUsr7.Location = new System.Drawing.Point(445, 314);
+            this.txtBox_NomUsr7.Name = "txtBox_NomUsr7";
+            this.txtBox_NomUsr7.Size = new System.Drawing.Size(139, 20);
+            this.txtBox_NomUsr7.TabIndex = 17;
+            this.txtBox_NomUsr7.Visible = false;
+            this.txtBox_NomUsr7.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtBox_NomUsr7_KeyUp);
+            // 
+            // txtBox_NomUsr8
+            // 
+            this.txtBox_NomUsr8.Location = new System.Drawing.Point(445, 344);
+            this.txtBox_NomUsr8.Name = "txtBox_NomUsr8";
+            this.txtBox_NomUsr8.Size = new System.Drawing.Size(139, 20);
+            this.txtBox_NomUsr8.TabIndex = 18;
+            this.txtBox_NomUsr8.Visible = false;
+            this.txtBox_NomUsr8.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtBox_NomUsr8_KeyUp);
+            // 
+            // txtBox_NomUsr9
+            // 
+            this.txtBox_NomUsr9.Location = new System.Drawing.Point(445, 374);
+            this.txtBox_NomUsr9.Name = "txtBox_NomUsr9";
+            this.txtBox_NomUsr9.Size = new System.Drawing.Size(139, 20);
+            this.txtBox_NomUsr9.TabIndex = 19;
+            this.txtBox_NomUsr9.Visible = false;
+            this.txtBox_NomUsr9.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtBox_NomUsr9_KeyUp);
+            // 
+            // btn_nav
+            // 
+            this.btn_nav.AutoSize = true;
+            this.btn_nav.Font = new System.Drawing.Font("Eras Demi ITC", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_nav.Location = new System.Drawing.Point(390, 416);
+            this.btn_nav.Name = "btn_nav";
+            this.btn_nav.Size = new System.Drawing.Size(164, 36);
+            this.btn_nav.TabIndex = 21;
+            this.btn_nav.Text = "Lets go boii !";
+            this.btn_nav.UseVisualStyleBackColor = true;
+            this.btn_nav.Visible = false;
+            this.btn_nav.Click += new System.EventHandler(this.btn_nav_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 464);
+            this.Controls.Add(this.btn_nav);
+            this.Controls.Add(this.txtBox_NomUsr9);
+            this.Controls.Add(this.txtBox_NomUsr8);
+            this.Controls.Add(this.txtBox_NomUsr7);
+            this.Controls.Add(this.txtBox_NomUsr6);
+            this.Controls.Add(this.txtBox_NomUsr5);
+            this.Controls.Add(this.txtBox_NomUsr4);
+            this.Controls.Add(this.txtBox_NomUsr3);
+            this.Controls.Add(this.txtBox_NomUsr2);
+            this.Controls.Add(this.lbl_NomUsr1);
+            this.Controls.Add(this.lbl_Usr9);
+            this.Controls.Add(this.lbl_Usr8);
+            this.Controls.Add(this.lbl_Usr7);
+            this.Controls.Add(this.lbl_Usr6);
+            this.Controls.Add(this.lbl_Usr5);
+            this.Controls.Add(this.lbl_Usr4);
+            this.Controls.Add(this.lbl_Usr3);
+            this.Controls.Add(this.lbl_Usr2);
+            this.Controls.Add(this.lbl_Usr1);
+            this.Controls.Add(this.lbl_Title);
+            this.Controls.Add(this.btn_start);
+            this.Name = "Login";
+            this.Text = "31Savage";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Label lbl_Title;
+        private System.Windows.Forms.Label lbl_Usr1;
+        private System.Windows.Forms.Label lbl_Usr2;
+        private System.Windows.Forms.Label lbl_Usr3;
+        private System.Windows.Forms.Label lbl_Usr4;
+        private System.Windows.Forms.Label lbl_Usr5;
+        private System.Windows.Forms.Label lbl_Usr6;
+        private System.Windows.Forms.Label lbl_Usr7;
+        private System.Windows.Forms.Label lbl_Usr8;
+        private System.Windows.Forms.Label lbl_Usr9;
+        private System.Windows.Forms.Label lbl_NomUsr1;
+        private System.Windows.Forms.TextBox txtBox_NomUsr2;
+        private System.Windows.Forms.TextBox txtBox_NomUsr3;
+        private System.Windows.Forms.TextBox txtBox_NomUsr4;
+        private System.Windows.Forms.TextBox txtBox_NomUsr5;
+        private System.Windows.Forms.TextBox txtBox_NomUsr6;
+        private System.Windows.Forms.TextBox txtBox_NomUsr7;
+        private System.Windows.Forms.TextBox txtBox_NomUsr8;
+        private System.Windows.Forms.TextBox txtBox_NomUsr9;
+        private System.Windows.Forms.Button btn_nav;
+    }
+}
+
---
+++ 31Savage/Login.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _31Savage
+{
+    public partial class Login : Form
+    {
+        int NbUsr;
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            btn_start.Visible = false;
+
+            lbl_Usr1.Visible = true;
+            lbl_NomUsr1.Visible = true;
+            lbl_Usr1.Enabled = false;
+            lbl_NomUsr1.Enabled = false;
+
+            lbl_Usr2.Visible = true;
+            txtBox_NomUsr2.Visible = true;
+        }
+
+        private void txtBox_NomUsr2_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && !string.IsNullOrEmpty(txtBox_NomUsr2.Text))
+            {
+                lbl_Usr2.Enabled = false;
+                txtBox_NomUsr2.Enabled = false;
+
+                lbl_Usr3.Visible = true;
+                txtBox_NomUsr3.Visible = true;
+
+                btn_nav.Visible = true;
+                NbUsr = 1;
+            }
+        }
+
+        private void txtBox_NomUsr3_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && !string.IsNullOrEmpty(txtBox_NomUsr3.Text))
+            {
+                lbl_Usr3.Enabled = false;
+                txtBox_NomUsr3.Enabled = false;
+
+                lbl_Usr4.Visible = true;
+                txtBox_NomUsr4.Visible = true;
+
+                NbUsr = 2;
+            }
+        }
+
+        private void txtBox_NomUsr4_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && !string.IsNullOrEmpty(txtBox_NomUsr4.Text))
+            {
+                lbl_Usr4.Enabled = false;
+                txtBox_NomUsr4.Enabled = false;
+
+                lbl_Usr5.Visible = true;
+                txtBox_NomUsr5.Visible = true;
+
+                NbUsr = 3;
+            }
+        }
+
+        private void txtBox_NomUsr5_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && !string.IsNullOrEmpty(txtBox_NomUsr5.Text))
+            {
+                lbl_Usr5.Enabled = false;
+                txtBox_NomUsr5.Enabled = false;
+
+                lbl_Usr6.Visible = true;
+                txtBox_NomUsr6.Visible = true;
+
+                NbUsr = 4;
+            }
+        }
+
+        private void txtBox_NomUsr6_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && !string.IsNullOrEmpty(txtBox_NomUsr6.Text))
+            {
+                lbl_Usr6.Enabled = false;
+                txtBox_NomUsr6.Enabled = false;
+
+                lbl_Usr7.Visible = true;
+                txtBox_NomUsr7.Visible = true;
+
+                NbUsr = 5;
+            }
+        }
+
+        private void txtBox_NomUsr7_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && !string.IsNullOrEmpty(txtBox_NomUsr7.Text))
+            {
+                lbl_Usr7.Enabled = false;
+                txtBox_NomUsr7.Enabled = false;
+
+                lbl_Usr8.Visible = true;
+                txtBox_NomUsr8.Visible = true;
+
+                NbUsr = 6;
+            }
+        }
+
+        private void txtBox_NomUsr8_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && !string.IsNullOrEmpty(txtBox_NomUsr8.Text))
+            {
+                lbl_Usr8.Enabled = false;
+                txtBox_NomUsr8.Enabled = false;
+
+                lbl_Usr9.Visible = true;
+                txtBox_NomUsr9.Visible = true;
+
+                NbUsr = 7;
+            }
+        }
+
+        private void txtBox_NomUsr9_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && !string.IsNullOrEmpty(txtBox_NomUsr9.Text))
+            {
+                lbl_Usr9.Enabled = false;
+                txtBox_NomUsr9.Enabled = false;
+
+                NbUsr = 8;
+            }
+        }
+
+        private void btn_nav_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
